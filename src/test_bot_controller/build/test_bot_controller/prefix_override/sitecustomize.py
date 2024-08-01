9d94a5974154c0784173d389af4a9c7d8bad5bf7
Elmeri Pohjois-Koivisto <elmeri.pohjois-koivisto@tuni.fi>
2024-08-01 07:57:01 +0000

    Delete accidental build files

--- src/test_bot_controller/build/test_bot_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/elmeripk/robots/src/test_bot_controller/install/test_bot_controller'
