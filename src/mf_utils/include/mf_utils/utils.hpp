444e4a1b1dce80ce35ffee6138df41f7c9cd90ac
Elmeri Pohjois-Koivisto <elmeripeekoo@gmail.com>
2025-06-25 15:30:51 +0300

    Convert the utils to plain CMake

--- src/mf_utils/include/mf_utils/utils.hpp
+++ src/mf_utils/include/mf_utils/utils.hpp
@@ -1,23 +1,23 @@
 #ifndef CONVERSION_UTILS_HPP
 #define CONVERSION_UTILS_HPP
 
+#include <cmath>
 #include <tuple>
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2/LinearMath/Matrix3x3.h"
 
+// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
 std::tuple<float, float, float> quaternion_to_euler(float x, float y, float z, float w){
 
-    auto quaternion = tf2::Quaternion(x, y, z, w);
-    
-    //https://robotics.stackexchange.com/questions/46421/transform-quaternion
-    // Convert quaternion into matrix
-    auto matrix = tf2::Matrix3x3(quaternion);
+    double sinr_cosp = 2 * (w * x + y * z);
+    double cosr_cosp = 1 - 2 * (x * x + y * y);
+    double roll = std::atan2(sinr_cosp, cosr_cosp);
 
-    double roll = 0.0;
-    double pitch = 0.0;
-    double yaw = 0.0;
-    // Get euler from the matrix by passing references and populating
-    matrix.getRPY(roll, pitch, yaw);
+    double sinp = std::sqrt(1 + 2 * (w * y - x * z));
+    double cosp = std::sqrt(1 - 2 * (w * y - x * z));
+    double pitch = 2 * std::atan2(sinp, cosp) - M_PI / 2;
+
+    double siny_cosp = 2 * (w * z + x * y);
+    double cosy_cosp = 1 - 2 * (y * y + z * z);
+    double yaw = std::atan2(siny_cosp, cosy_cosp);
 
     return {static_cast<float>(roll), static_cast<float>(pitch), static_cast<float>(yaw)};
 
