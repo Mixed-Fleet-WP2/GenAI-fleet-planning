ec1c52444b25dda9f113f8d897fd5203dff1e27c
Elmeri Pohjois-Koivisto <elmeripeekoo@gmail.com>
2025-06-25 19:22:15 +0300

    Add composed implementation

--- src/state_bridge/launch/state_bridge_launch.py
+++ src/state_bridge/launch/state_bridge_launch.py
@@ -4,6 +4,10 @@ from launch_ros.actions import Node
 import os
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from ros_gz_bridge.actions import RosGzBridge
+
 
 def generate_launch_description():
 
@@ -24,13 +28,13 @@ def generate_launch_description():
         description="Path to gz bridge configuration"
     )
 
-    mqtt_bridge = Node(
-        name="global_mqtt_client",
-        package='mqtt_client',
-        executable='mqtt_client',
-        output='screen',
-        parameters=[mqtt_config_file]
-    )
+    # mqtt_bridge = Node(
+    #     name="global_mqtt_client",
+    #     package='mqtt_client',
+    #     executable='mqtt_client',
+    #     output='screen',
+    #     parameters=[mqtt_config_file]
+    # )
 
     state_bridge = Node(
         package='state_bridge',
@@ -38,10 +42,10 @@ def generate_launch_description():
         output='screen'
     )
 
-    gz_bridge = Node(
+    gz_bridge1 = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        name="global_gz_bridge",
+        name="globall_gz_bridge",
         parameters=[
             {
                 'config_file': gz_bridge_config_file,
@@ -52,13 +56,58 @@ def generate_launch_description():
         output='screen',
     )
 
+    ros_gz_bridge_action = RosGzBridge(
+        bridge_name="test_bridge",
+        config_file=gz_bridge_config_file,
+        container_name="sim_env_container",
+        create_own_container=str(False),
+        use_composition=str(True)
+        #bridge_params=LaunchConfiguration('bridge_params'),
+    )
+
+
+    mqtt_bridge = ComposableNode(
+        package="mqtt_client",
+        plugin="mqtt_client::MqttClient",
+        name="global_mqtt_client",
+        parameters=[mqtt_config_file],
+    )
+
+    gz_bridge = ComposableNode(
+        package="ros_gz_bridge",
+        plugin="ros_gz_bridge::RosGzBridge",
+        name="global_gz_bridge",
+        parameters=[
+            {
+                'config_file': gz_bridge_config_file,
+                'expand_gz_topic_names': True,
+                'use_sim_time': True,
+            }
+        ],
+    )
+    # https://github.com/gazebosim/ros_gz/pull/528
+    # https://github.com/gazebosim/ros_gz/blob/bba6783a85955e2719a0d11468d9a8a79223b0a7/ros_gz_bridge/ros_gz_bridge/actions/ros_gz_bridge.py#L35
+    container = ComposableNodeContainer(
+        name='sim_env_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        output='both',
+        composable_node_descriptions= [
+            mqtt_bridge,
+        ],
+        
+    )
+
 
     ld = LaunchDescription()
     ld.add_action(declare_mqtt_config_file)
     ld.add_action(declare_gz_bridge_path)
-    ld.add_action(gz_bridge)
-    ld.add_action(mqtt_bridge)
+    #ld.add_action(gz_bridge1)
+    # ld.add_action(mqtt_bridge)
     ld.add_action(state_bridge)
+    ld.add_action(container)
+    ld.add_action(ros_gz_bridge_action)
 
     return ld
 
