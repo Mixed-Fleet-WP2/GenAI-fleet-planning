4cb5e0cd4f542c1879e6e1d88ae612935f582d1a
Elmeri Pohjois-Koivisto <elmeripeekoo@gmail.com>
2026-04-09 10:18:09 +0300

    Start implementing multi-stage build for test pkg

--- src/test_pkg/setup.py
+++ src/test_pkg/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'test_pkg'
 
@@ -6,10 +7,14 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    # First item is the destination, second item is the source
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*')),
+        ('share/' + package_name + '/config', glob('config/*')),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
